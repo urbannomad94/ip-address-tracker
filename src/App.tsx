ed79ac73b218f211f7b1888b8f7247ddacdf2d0c
Aidan Williams <aidanw4@gmail.com>
2023-06-05 13:41:53 +0700

    remove gmap api and use geocode to obtain lat and log coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,25 @@ const App = () => {
     const [data, setData] = useState();
     const [ipAddress, setIpAddress] = useState('');
     const [submitted, setSubmitted] = useState(false);
+    // const [location, setLocation] = useState([null, null])
 
     const initialDataFetch = async() => {
-      const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_API_KEY}`);
-      return res.json();
+      const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_GEO_API_KEY}`);
+      const data = await res.json();
+      return data;
     }
   
     useEffect(() => {
       initialDataFetch()
-        .then(res => setData(res))
+        .then(res => {
+            setData(res)
+            // setLocation([res.location.region, res.location.country])
+            setIpAddress(res.ip)
+        })
     }, [])
 
     const dataFetch = async() => {
-        const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_API_KEY}&ipAddress=${ipAddress}`);
+        const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_GEO_API_KEY}&ipAddress=${ipAddress}`);
         return res.json();
     }
 
@@ -33,7 +39,7 @@ const App = () => {
         <>
             <IPSearch setIpAddress={setIpAddress} ipAddress={ipAddress} setSubmitted={setSubmitted} />
             {data ? <SearchResults data={data} /> : null}
-            <Map />
+            {data ? <Map data={data} /> : null}
         </>
     )
 }
--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,13 +1,16 @@
+import { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 
-const Map = () => {
+const Map = ({data}) => {
+  let lat = data.location.lat
+  let lng = data.location.lng
   return (
-    <MapContainer className='map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className='map' center={[lat, lng]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[lat, lng]}>
             <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
